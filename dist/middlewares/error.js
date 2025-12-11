0c6d47515942b851a7037f4d4c1aefe35016d221
Theodoric <daviderickd7@hotmail.com>
2025-12-11 10:02:46 -0300

    Delete dist directory for cleanup
    If one needs it, they shall build this package through `tsc` or `npx tsc` on root.

--- dist/middlewares/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (err, req, res, next) => {
-    console.error(err);
-    res.status(err.status_code || 500).json({
-        code: err.status_code || 500,
-        message: err.message || "Internal Server Error (unspecified)"
-    });
-};
